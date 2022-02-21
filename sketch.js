e7c4d63f2ada05daec18c4afe2bf42fd3240cae7
Kristian Virtanen <krisu.virtanen@gmail.com>
2022-02-21 19:26:23 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,8 +26,8 @@ function setup() {
 	highscore = 0;
 	gameOverFrames = 0;
 	
-	createCanvas(innerWidth, innerHeight * 0.7)
-	//createCanvas(_width, _heigth);
+	//createCanvas(innerWidth, innerHeight * 0.7)
+	createCanvas(_width, _heigth);
 	frameRate(_fps);
 	textSize(32);
 	sounds[_bgSongSnd].play();
